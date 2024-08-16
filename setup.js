5470ce73793de457ccab055df02a8ade8a83ea16
kanaxz <kanaxstratz@gmail.com>
2024-08-16 19:11:46 +0200

    first commit

---
+++ setup.js
@@ -0,0 +1,15 @@
+const setup = require('sools-modeling-hedera/setup')
+const context = require('sools-core-client/context')
+
+setup.routing.actions.push({
+  url: '/jwt',
+  content: '<i class="fa-solid fa-key"></i>',
+  check(user) {
+    if (context.user !== user) {
+      throw new Error('User not matching')
+    }
+  },
+  async execute(req, res, next) {
+    await res.page(import('./pages/Jwt'))
+  }
+})
